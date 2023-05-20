079d8ac0f3839bd938362e8a6256fb9fe845b002
Rahul <rsc120@nist.edu>
2023-05-20 16:47:56 +0530

    Commited

--- Components/Card.jsx
+++ Components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Card = ({ allcampaign, setOpenModel, setDonate, title }) => {
-  console.log(allcampaign);
+const Card = ({ allCampaigns, setOpenModel, setDonateCampaign, title }) => {
+  console.log(allCampaigns);
   const daysleft = (deadline) => {
     const difference = new Date(deadline).getTime() - Date.now();
     const remainingDays = difference / (1000 * 3600 * 24);
@@ -13,7 +13,7 @@ const Card = ({ allcampaign, setOpenModel, setDonate, title }) => {
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-xl-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <p className="py-16 text-2xl font-bold leading-5">{title}</p>
         <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-          {allcampaign?.map((campaign, i) => {
+          {allCampaigns?.map((campaign, i) => {
             return (
               <div
                 onClick={() => (setDonate(campaign), setOpenModel(true))}
